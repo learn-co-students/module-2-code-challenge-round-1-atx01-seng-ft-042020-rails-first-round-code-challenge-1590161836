5586845d313a720b146d3cdeae470f735bfd2d37
agomezdemen <agomezdemen15@gmail.com>
2020-05-22 14:12:00 -0500

    submission for deadline

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+    has_many :heroinepowers
+    has_many :powers, through: :heroinepower
+
+    validates :name, :super_name, :presence => true, allow_blank: false
+    validates :name, :super_name, :uniqueness => true
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,4 @@
 class Power < ApplicationRecord
+    has_many :heroinepower
+    has_many :heroines, through: :heroinepower
 end
