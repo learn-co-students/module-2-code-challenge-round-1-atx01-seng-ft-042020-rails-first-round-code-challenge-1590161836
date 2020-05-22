a0067ce5ff2cc569a9f951628e854b123ea09d6e
rhinehardtc <rhinehardtc@gmail.com>
2020-05-22 14:12:03 -0500

    Submission for deadline

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,8 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+    accepts_nested_attributes_for :powers
+
+    validates :name, :super_name, presence: true
+    validates :super_name, uniqueness: true
 end
