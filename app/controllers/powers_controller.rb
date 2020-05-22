7bc3e98bee38a932d40413ca63484e3e8d93c186
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-05-22 14:11:56 -0500

    submission for deadline

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,35 @@
 class PowersController < ApplicationController
+
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.update(power_params)
+
+    if !params[:power][:heroine_ids].empty?
+      @power.heroines.clear
+      params[:power][:heroine_ids].each do |id|
+        @power.heroines << Heroine.find(id)
+      end
+      @power.save
+    end
+    
+    redirect_to power_path(@power)
+  end
+
+
+  def power_params
+    params.require(:power).permit(:name, :description, :heroine_ids)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,22 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+    
+    validates :name, :super_name, presence: true
+    validate :super_name_is_unique
+
+    def power_id=(id)
+        self.powers << Power.find(id)
+    end
+
+    def power_id
+    end
+
+    def super_name_is_unique
+        heroines = Heroine.where(super_name: self.super_name).select{|heroine| heroine.id != self.id}
+        if !heroines.empty?
+            errors.add(:super_name, "must be unique")
+        end
+    end
+
 end
