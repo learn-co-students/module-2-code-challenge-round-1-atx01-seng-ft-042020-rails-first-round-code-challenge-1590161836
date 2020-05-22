cf9dedd6267dbee00e24972e002979799a91cf84
marshall405 <marshall.slemp@gmail.com>
2020-05-22 14:11:53 -0500

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: :show
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new 
+    @powers = Power.all 
+  end
+
+  def create
+    puts params
+    @heroine = Heroine.new heroine_params
+    if @heroine.valid?
+      @heroine.save
+      HeroinePower.create(power_id: params[:heroine][:power_ids], heroine_id: @heroine.id)
+      redirect_to heroine_path @heroine
+    else 
+      @powers = Power.all
+      render :new 
+    end
+  end
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,36 @@
 class PowersController < ApplicationController
+
+  before_action :set_power, only: [:show, :edit, :update]
+
   def index
     @powers = Power.all
   end
+
+  def edit
+    @heroines = Heroine.all 
+  end
+
+  def update
+    @power.update power_params
+    params[:power][:heroine_ids].each do |heroine_id|
+      @heroine = Heroine.find_by_id(heroine_id)
+      if @heroine
+        @heroine.powers << @power
+      end
+    end
+
+    redirect_to power_path @power
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
+
+  def set_power
+    @power = Power.find(params[:id])
+  end
+
 end
